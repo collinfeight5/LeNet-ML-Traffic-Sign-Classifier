0c60efcc5f752b975b88f43779911a8e750696aa
Collin.Feight <Collin.Feight@PACCAR.com>
2019-04-09 12:50:03 -0700

    first upload

--- README.md
+++ README.md
@@ -1,1 +1,133 @@
-# LeNet-ML-Traffic-Sign-Classifier
+**Build a Traffic Sign Recognition Project**
+
+The goals / steps of this project are the following:
+* Load the data set (see below for links to the project data set)
+* Explore, summarize and visualize the data set
+* Design, train and test a model architecture
+* Use the model to make predictions on new images
+* Analyze the softmax probabilities of the new images
+* Summarize the results with a written report
+
+
+[//]: # (Image References)
+
+[image1]: ./Output_Images/Data_Exploration_result.png "Visualization"
+[image2]: ./Output_Images/Pre_Processing_Result.png "PreProcessing"
+[image3]: ./new_images/Image1.png "Random Noise"
+[image4]: ./new_images/Image2.png "Traffic Sign 1"
+[image5]: ./new_images/Image3.png "Traffic Sign 2"
+[image6]: ./new_images/Image4.png "Traffic Sign 3"
+[image7]: ./new_images/Image5.png "Traffic Sign 4"
+[image8]: ./new_images/Image6.png "Traffic Sign 5"
+[image9]: ./Output_Images/Softmax_TK2_Values.png "Visualization"
+[image10]: ./Output_Images/Guess_Success_Softmax.png "Visualization"
+
+---
+### Writeup / README
+
+#### 1. Provide a Writeup / README that includes all the rubric points and how you addressed each one. You can submit your writeup as markdown or pdf. You can use this template as a guide for writing the report. The submission includes the project code.
+
+### Data Set Summary & Exploration
+
+#### 1. Provide a basic summary of the data set. In the code, the analysis should be done using python, numpy and/or pandas methods rather than hardcoding results manually.
+
+* The size of training set is 34799
+* The size of the validation set is 4410
+* The size of test set is 12630
+* The shape of a traffic sign image is (32,32,3) corresponding to RGB images
+* The number of unique classes/labels in the data set is 43
+
+#### 2. Include an exploratory visualization of the dataset.
+This is done in lines 67-77. 
+
+Here is an exploratory visualization of the data set. It shows a collection of random traffic signs and the corresponding labels that they have. Note that the labels can be attached to traffic stop sign names using the signames.csv file that it apart of the data set.
+
+
+![alt text][image1]
+
+### Design and Test a Model Architecture
+
+#### 1. Describe how you preprocessed the image data. What techniques were chosen and why did you choose these techniques? Consider including images showing the output of each preprocessing technique. Pre-processing refers to techniques such as converting to grayscale, normalization, etc. (OPTIONAL: As described in the "Stand Out Suggestions" part of the rubric, if you generated additional data for training, describe why you decided to generate additional data, how you generated the data, and provide example images of the additional data. Then describe the characteristics of the augmented training set like number of images in the set, number of images for each class, etc.)
+This is done in lines 81-93. 
+As a first step, I decided to convert the images to grayscale because the CNN was able to learn from analyzing the shapes. I also normalized the data because it improves efficency processing images. 
+
+Here is an example of a traffic sign image before and after grayscaling/normalzing.
+
+![alt text][image2]
+
+#### 2. Describe what your final model architecture looks like including model type, layers, layer sizes, connectivity, etc.) Consider including a diagram and/or table describing the final model.
+My architecture can be found in lines 107-157. 
+
+My final model consisted of the following layers:
+
+| Layer         		|     Description	        					| 
+|:---------------------:|:---------------------------------------------:| 
+| Input         		| 32x32x3 RGB image   							| 
+| Convolution 3x3     	| 1x1 stride, outputs 28x28x12 	|
+| RELU					|												|
+| Max pooling	      	| 2x2 stride,  outputs 14x14x12 				|
+| Convolution 3x3	    | 1x1 stride, outputs 10x10x25      									|
+| RELU   |             |                                    
+| Max Pooling        | 2x2 stride, outputs 5x5x25          |
+| Flatten 		| outputs 625        									|
+| Dropout				| outputs 625        									|
+| Linear						|		outputs 300										|
+|	Linear					|		Outputs 100										|
+| Linear     | outputs 43           |
+ 
+
+
+#### 3. Describe how you trained your model. The discussion can include the type of optimizer, the batch size, number of epochs and any hyperparameters such as learning rate.
+
+The Training pipeline can be found in lines 190-221. Note that placeholder variables are elsewhere (160-164). 
+
+To train the model, I used the AdamOptimizer with a learning rate of .001, batch size of 64, and a total of 15 epochs. These were tested and tunned to get the best results. 
+
+#### 4. Describe the approach taken for finding a solution and getting the validation set accuracy to be at least 0.93. Include in the discussion the results on the training, validation and test sets and where in the code these were calculated. Your approach may have been an iterative process, in which case, outline the steps you took to get to the final solution and why you chose those steps. Perhaps your solution involved an already well known implementation or architecture. In this case, discuss why you think the architecture is suitable for the current problem.
+ Printing the results and calling the 'eval' function can be found in 256-267. Note that this also includes evaluating the new road signs that were downloaded from the website. 
+ 
+My final model results were:
+* training set accuracy of 97.7%
+* validation set accuracy of 95.3%
+* test set accuracy of 94.3%
+
+I decided to go with a well-known architecure model by using the LeNet model. I believed it would be good for several reasons. One of them being the LeNet architecture is known to be good for handling image processing. Another is the fact that we learned this method during our lessons so I was more familiar with it and understood more about what was happening behind the scenes of the model. I did apply the dropout method to the model, which slightly improved the results. 
+
+### Test a Model on New Images
+
+#### 1. Choose five German traffic signs found on the web and provide them in the report. For each image, discuss what quality or qualities might be difficult to classify.
+
+Here are six German traffic signs that I found on the web:
+
+![alt text][image3] ![alt text][image4] ![alt text][image5] 
+![alt text][image6] ![alt text][image7] ![alt text][image8]
+
+In general, I picked these images due to them being on the more depective, clearer side of things. I expect my model should do a good job of classifying these specific images that I selected. 
+
+#### 2. Discuss the model's predictions on these new traffic signs and compare the results to predicting on the test set. At a minimum, discuss what the predictions were, the accuracy on these new predictions, and compare the accuracy to the accuracy on the test set (OPTIONAL: Discuss the results in more detail as described in the "Stand Out Suggestions" part of the rubric).
+
+The results of analyzing the new images was 100% for the 6 test images that I looked at. This is better than the results of the test set, but this is most likely due to the specific images that were selected. 
+
+
+#### 3. Describe how certain the model is when predicting on each of the five new images by looking at the softmax probabilities for each prediction. Provide the top 5 softmax probabilities for each image along with the sign type of each probability. (OPTIONAL: as described in the "Stand Out Suggestions" part of the rubric, visualizations can also be provided such as bar charts)
+this can be found in lines 271-293.
+
+The result of the softmax TK2 applied to the new test images can be seen in the image below:
+
+
+
+![alt text][image9]
+
+
+
+The guess success rate can be seen in the following image:
+
+![alt text][image10]
+
+
+
+As can be seen, the model was able to correctly identify the images that it was given. Like I described earlier, this could be due to the easier images that were used as the test images. 
+
+
+
+
---
+++ Traffic_Sign_Classifier.py
@@ -0,0 +1,293 @@
+import tensorflow as tf
+import pickle
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
+import time
+import glob
+import cv2
+
+
+### Read in Data
+training_file = 'train.p'
+validation_file = 'valid.p'
+testing_file = 'test.p'
+
+with open(training_file, mode='rb') as f:
+    train = pickle.load(f)
+with open(validation_file, mode='rb') as f:
+    valid = pickle.load(f)
+with open(testing_file, mode='rb') as f:
+    test = pickle.load(f)
+
+# Assign Data to variables
+X_train, y_train = train['features'], train['labels']
+X_valid, y_valid = valid['features'], valid['labels']
+X_test, y_test = test['features'], test['labels']
+
+
+# Number of training examples
+n_train = X_train.shape[0]
+
+# Number of validation examples
+n_validation = X_valid.shape[0]
+
+# Number of testing examples.
+n_test = X_test.shape[0]
+
+# shape of an traffic sign image
+image_shape = X_train.shape[1:]
+
+# unique classes/labels there are in the dataset.
+n_classes = len(set(y_train))
+
+print("Number of training examples =", n_train)
+print("Number of validation examples =", n_validation)
+print("Number of testing examples =", n_test)
+print("Image data shape =", image_shape)
+print("Number of classes =", n_classes)
+
+
+def plot(figures, rows, columns, labels=None):
+    fig, axs = plt.subplots(ncols=columns, nrows=rows, figsize=(12, 14))
+    axs = axs.ravel()
+    for index, title in zip(range(len(figures)), figures):
+        axs[index].imshow(figures[title], plt.gray())
+        if (labels != None):
+            axs[index].set_title(labels[index])
+        else:
+            axs[index].set_title(title)
+
+        axs[index].set_axis_off()
+
+    plt.tight_layout()
+
+
+### Data exploration visualization code goes here.
+# Visualizations will be shown in the notebook.
+# fig, axs = plt.subplots(3,5, figsize=(15, 6))
+# fig.subplots_adjust(hspace = .2, wspace=.001)
+# axs = axs.ravel()
+# for i in range(15):
+#     index = random.randint(0, len(X_train))
+#     image = X_train[index]
+#     axs[i].axis('off')
+#     axs[i].imshow(image)
+#     axs[i].set_title(y_train[index])
+
+
+#Pre-Processing the data --> convert to grayscale and normalize
+def preprocess(Data):
+    # axis == rgb
+    X_Train_Gray = np.sum(Data / 3, axis=3, keepdims=True)
+    features_train = (X_Train_Gray-128)/128
+    return features_train
+
+# Organize data so everything in terms of features and label
+
+
+features_train = preprocess(X_train)
+features_valid = preprocess(X_valid)
+features_test = preprocess(X_test)
+labels_train = y_train
+labels_valid = y_valid
+labels_test = y_test
+
+
+### Convolution Using LeNet model
+# 'Static' Parameters
+mu = 0
+sigma = .1
+kernel_size = [1,2,2,1]
+pool_strides = [1, 2, 2, 1]
+padding_str = 'VALID'
+
+
+def convolution(data, length_in, depth_in, length_out, depth_out):
+    filter_length = length_in-length_out + 1
+    stride = [1,1,1,1]
+    W = tf.Variable(tf.truncated_normal(shape=(filter_length, filter_length, depth_in, depth_out), mean=mu, stddev=sigma))
+    b = tf.Variable(tf.zeros(depth_out))
+    #Using Valid Padding method to calculate filter dimensions
+    model = tf.nn.conv2d(data, W, strides=stride, padding=padding_str) + b
+    #using relu as activation function (non-linear)
+    return tf.nn.relu(model)
+
+
+#Linear function used for fully connected layers
+def linear(data, length_in, length_out):
+    W = tf.Variable(tf.truncated_normal(shape=(length_in, length_out), mean=mu, stddev=sigma))
+    b = tf.Variable(tf.zeros(length_out))
+    model = tf.matmul(data,W) + b
+    return model
+
+
+# Generates Logits and calls convolution and linear functions
+def Pipeline(data, keep_prob):
+
+    #Layer One
+    #32x32x1 --> 28x28x12
+    conv1 = convolution(data, 32, 1, 28, 12)
+
+    #28x28x12 --> 14x14x12
+    conv1 = tf.nn.max_pool(conv1, ksize=kernel_size, strides=pool_strides, padding=padding_str)
+
+    #Layer Two
+    #14x14x12 --> 10x10x25
+    conv2 = convolution(conv1, 14, 12, 10, 25)
+
+    #10x10x25 --> 5x5x25
+    conv2 = tf.nn.max_pool(conv2, ksize=kernel_size, strides=pool_strides, padding=padding_str)
+
+    #Layer Three
+    flat = tf.contrib.layers.flatten(conv2)
+    dropped = tf.nn.dropout(flat, keep_prob)
+    final_3 = linear(dropped, 625, 300)
+    final_3 = tf.nn.relu(final_3)
+
+    #Layer Four
+    final_4 = linear(final_3, 300, 100)
+    final_4 = tf.nn.relu(final_4)
+
+    #Layer Five
+    final_layer = linear(final_4, 100, n_classes)
+
+    return final_layer
+
+
+# Create Placeholders that will be used in the training/eval functions. These placeholders are what get updated
+feature_shape = features_train.shape[1:]
+x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.int32, None)
+one_hot = tf.one_hot(y, n_classes)
+keep_prob = tf.placeholder(tf.float32)
+
+logits = Pipeline(x, keep_prob)
+
+# Tuning parameters that effect the rate and amount the model trains
+epoch = 15
+batch_size = 64
+rate = .001
+
+### Evaluation Pipeline
+prediction_inc = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot, 1))
+accuracy_inc = tf.reduce_mean(tf.cast(prediction_inc, tf.float32))
+
+# Make sure keep_prob stays one here for testing
+def eval(X, Y):
+    num_list = len(X)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_list, batch_size):
+        batch_x, batch_y = X[offset:offset+batch_size], Y[offset:offset+batch_size]
+        accuracy = sess.run(accuracy_inc, feed_dict={x: batch_x, y: batch_y, keep_prob: 1.0})
+        total_accuracy += (accuracy*len(batch_x))
+    return total_accuracy / num_list
+
+
+### Training pipeline
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=one_hot)
+loss = tf.reduce_mean(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=rate).minimize(loss)
+
+# Used to save information that has been updated such as the weight values
+saver = tf.train.Saver()
+model_file = './model'
+
+
+def train(X, Y, prob):
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print('Let the training begin')
+        for i in range(epoch):
+
+            # train test split faster but slightly worse results than shuffling training set
+
+            # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=int(time.time()))
+            X_train, Y_train = shuffle(X,Y)
+            begin_time = time.time()
+            for offset in range(0, len(X_train), batch_size):
+                end = offset + batch_size
+                features, labels = X_train[offset:end], Y_train[offset:end]
+                sess.run(train_step, feed_dict={x: features, y: labels, keep_prob: prob})
+            valid = eval(X_train, Y_train)
+            #valid = eval(X_test, Y_test)
+            print("[{3:.1f}s] epoch {0}/{1}: validation = {2:.3f}".format(i + 1, epoch, valid,
+                                                                              time.time() - begin_time))
+            # save weight scores after each epoch iteration
+            saver.save(sess, model_file)
+            print("model saved to {}".format(model_file))
+
+
+#### TRAIN DATA FUNCTION CALLED HERE
+
+#train(features_train, labels_train, .5)
+
+####
+# Loading in New Images from website
+new_images = sorted(glob.glob('new_images/Image*.png'))
+new_labels = np.array([1,22,35,15,37,18])
+figures = {}
+labels = {}
+my_new_signs = []
+index = 0
+name_values = np.genfromtxt('signnames.csv', skip_header=1, dtype=[('myint','i8'), ('mysring','S55')], delimiter=',')
+for image in new_images:
+    img = cv2.cvtColor(cv2.imread(image), cv2.COLOR_BGR2RGB)
+    my_new_signs.append(img)
+    figures[index] = img
+    labels[index] = name_values[new_labels[index]][1].decode('ascii')
+    index += 1
+#plot(figures, 3, 2, labels)
+
+my_new_signs = np.array(my_new_signs)
+gray_new_signs = preprocess(my_new_signs)
+
+stop = 6
+figures = {}
+labels = {}
+for i in range(stop):
+    labels[i] = name_values[new_labels[i]][1].decode('ascii')
+    figures[i] = gray_new_signs[i].squeeze()
+plot(figures, 3, 2, labels)
+
+## Runs evaluation pipeline on validation and test data to determine how well model was trained
+with tf.Session() as sess:
+    #restores saved weights so dont have to retrain model before using on validation/testing/new image datasets
+    saver.restore(sess, model_file)
+    train = eval(features_train, labels_train)
+    valid = eval(features_valid, labels_valid)
+    test = eval(features_test, labels_test)
+    print("accuracy in validation set: {:.3f}".format(train))
+
+    print("accuracy in validation set: {:.3f}".format(valid))
+    print("accuracy in test set: {:.3f}".format(test))
+    my_accuracy = eval(gray_new_signs, new_labels)
+    print("My Data Set Accuracy = {:.3f}".format(my_accuracy))
+
+
+# Softmax Probabilites
+k_size = 5
+softmax_logits = tf.nn.softmax(logits)
+top_k = tf.nn.top_k(softmax_logits, k=k_size)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    #     my_saver = tf.train.import_meta_graph('./lenet.meta')
+    saver.restore(sess, model_file)
+    my_softmax_logits = sess.run(softmax_logits, feed_dict={x: gray_new_signs, keep_prob: 1.0})
+    my_top_k = sess.run(top_k, feed_dict={x: gray_new_signs, keep_prob: 1.0})
+    print(my_top_k)
+
+    for i in range(6):
+        figures = {}
+        labels = {}
+
+        figures[0] = new_images[i]
+        labels[0] = "Original"
+
+        for j in range(k_size):
+            print('Guess {} : ({:.0f}%)'.format(j+1, 100*my_top_k[0][i][j]))
+            labels[j + 1] = 'Guess {} : ({:.0f}%)'.format(j + 1, 100 * my_top_k[0][i][j])
+            figures[j + 1] = X_valid[np.argwhere(y_valid == my_top_k[1][i][j])[0]].squeeze()
